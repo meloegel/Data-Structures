15ac65a2f229cc88d89e339b75509d640a93ccfe
meloegel <meloegel@gmail.com>
2020-08-12 17:57:25 -0400

    day 3

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -14,23 +14,75 @@ class BSTNode:
         self.value = value
         self.left = None
         self.right = None
+        self.length = 0
 
     # Insert the given value into the tree
+    # w/ recursion
+    # def insert(self, value):
+    #     if value < self.value:
+    #         if self.left:
+    #             self.left.insert(value)
+    #         else:
+    #             self.left = BSTNode(value)
+    #     else:
+    #         if self.right:
+    #             self.right.insert(value)
+    #         else:
+    #             self.right = BSTNode(value)
+    # w/o recursion
     def insert(self, value):
-        pass
+        current_node = self
+        while current_node is not None:
+            if value < current_node.value:
+                if current_node.left is None:
+                    current_node.left = BSTNode(value)
+                    current_node.length += 1
+                    return
+                else:
+                    current_node = current_node.left
+            else:
+                if current_node.right is None:
+                    current_node.right = BSTNode(value)
+                    current_node.length += 1
+                    return
+                else:
+                    current_node = current_node.right
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        current_node = self
+        while current_node is not None:
+            if target < current_node.value:
+                if current_node.left is None:
+                    return False
+                else:
+                    current_node = current_node.left
+            elif target > current_node.value:
+                if current_node.right is None:
+                    return False
+                else:
+                    current_node = current_node.right
+            else:
+                return True
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        current_node = self
+        while current_node is not None:
+            if current_node.right is None:
+                return current_node.value
+            else:
+                current_node = current_node.right
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        if self is None:
+            return
+        else:
+            left_tree = BSTNode.for_each(self.left, fn)
+            right_tree = BSTNode.for_each(self.right, fn)
+            return fn(self.value)
 
     # Part 2 -----------------------
 
@@ -63,7 +115,7 @@ class BSTNode:
 """
 This code is necessary for testing the `print` methods
 """
-bst = BinarySearchTree(1)
+bst = BSTNode(1)
 
 bst.insert(8)
 bst.insert(5)
@@ -76,10 +128,10 @@ bst.insert(2)
 bst.bft_print()
 bst.dft_print()
 
-print("elegant methods")
-print("pre order")
-bst.pre_order_dft()
-print("in order")
-bst.in_order_dft()
-print("post order")
-bst.post_order_dft()  
+# print("elegant methods")
+# print("pre order")
+# bst.pre_order_dft()
+# print("in order")
+# bst.in_order_dft()
+# print("post order")
+# bst.post_order_dft()  
